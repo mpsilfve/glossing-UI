ec5d5518da6b9a6b1b62856d8671103db1c586af
Konstantin Mestnikov <64804506+ritec03@users.noreply.github.com>
2021-03-29 21:45:06 -0700

    added ids to files based on time

--- backend_server/server.py
+++ backend_server/server.py
@@ -1,63 +1,10 @@
 import flask, sys, json
+from datetime import datetime
 from flask import request, jsonify
 
 
 app = flask.Flask(__name__) # create the Flask application object 
 app.config["DEBUG"] = True # in debugger mode you see specific errors 
-
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
-@app.route('/', methods=['GET']) # we have mapped / path to home. whatever is mapped to / just gets displayed to browser right away. this is called routing 
-# get requests are to the user, post requests are from the user. 
-def home():
-    return "<h1>Distant Cuddling Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-# instead of returning HTML, model server will return data in the form af JSON file. 
-
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args: # what are request args? those are the query parameter : after "?"
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
 
 @app.route('/api/job', methods=['POST', 'GET'])
 def api_job():
@@ -102,8 +49,11 @@ def api_job():
         """
     else:
         data = request.json
+        now = datetime.now()
+        request_id = int(datetime.timestamp(now))
+        data["id"] = request_id
         print(data["text"], file=sys.stderr)
-        with open('/data/data.txt', 'w') as outfile:
+        with open(f'/data/{request_id}.txt', 'w') as outfile:
             json.dump(data, outfile)
 
         return "<h1>Post request</h1>"
