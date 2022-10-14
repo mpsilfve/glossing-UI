45941e37ab9d8143285249a23ad05bba9b1295f0
Anna <annajs@student.ubc.ca>
2022-10-14 10:41:27 -0700

    Made all 3 f.model files (for coling) download from the project's google drive account. 	- This means the files will no longer be stored in the repo.

--- backend_coling/entrypoint.sh
+++ backend_coling/entrypoint.sh
@@ -5,9 +5,26 @@ if [[ ! -f Word_cls/f.model ]]
 then
     pip install gdown
     cd Word_cls
-    gdown "https://drive.google.com/uc?id=1rbsl-bjOpNct5e2tYX8qXwIahoklj5KN"
+    gdown "https://drive.google.com/uc?id=1bmi6b5QkMQXOqwt7Qfi3cUT356Pm2HSm"
     cd ..
 fi
+
+if [[ ! -f Word_dumb/f.model ]]
+then
+    pip install gdown
+    cd Word_dumb
+    gdown "https://drive.google.com/uc?id=1AVeR7Zqs7tKg6PFVMmlmlU6fAJRjjldp"
+    cd ..
+fi
+
+if [[ ! -f Word_smart/f.model ]]
+then
+    pip install gdown
+    cd Word_smart
+    gdown "https://drive.google.com/uc?id=1XU_bpSZMT16wN5z3axOsJ0ru__FIbph2"
+    cd ..
+fi
+https://drive.google.com/file/d/1XU_bpSZMT16wN5z3axOsJ0ru__FIbph2/view?usp=sharing
 pwd
 cd ..
 
